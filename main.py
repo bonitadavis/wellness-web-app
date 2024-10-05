1bfc9722e23d61b7cc0ff3018a640b73e3f577a9
Matthew Benedict <46784000+UInt2048@users.noreply.github.com>
2024-10-05 23:11:36 +0000

    Add goal form

--- main.py
+++ main.py
@@ -8,12 +8,26 @@ def get_data():
      {"title": "Python hello", "days": random.randint(0, 7), "limit": 7, "type": "physical"},
      {"title": "Mental health goal", "days": random.randint(0, 7), "limit": 7, "type": "mental"}]
 
+@app.route("/add", methods=['GET', 'POST'])
+def add_data():
+    """Do something to add a goal"""
+    name = request.args['name']
+    goal_type = request.args['type']
+    days = []
+    for i in range(0, 7):
+        days.append(request.args["d%d" % i] == 0)
+    return add_data_internal(name, goal_type, days)
+
+def add_data_internal(name: str, goal_type: str, days: list[bool]):
+    print("We're adding {} {} {}".format(name, goal_type, days))
+    return main()
+
 @app.route("/update", methods=['GET', 'POST'])
 def update_data():
     """Do something to update data"""
     return update_data_internal(int(request.args['goal']))
 
-def update_data_internal(goal):
+def update_data_internal(goal: int):
     print("We're updating goal number %d" % goal)
     return main()
 
