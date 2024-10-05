e7448f54dc27a1a06720555c573c569edda2d4cf
Matthew Benedict <46784000+UInt2048@users.noreply.github.com>
2024-10-05 20:16:15 +0000

    Ring demo

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 from flask import Flask, render_template
+import random
 
 app = Flask(__name__)
 
 def get_posts():
-     return [{"title": "Python hello", "created": "3"}]
+     return [{"title": "Python hello", "days": random.randint(0, 7), "limit": 7}]
+
+@app.route("/update", methods=['GET', 'POST'])
+def update_posts():
+    """Do something to update posts"""
+    print("We're updating this post!")
+    return main()
 
 @app.route("/")
 def main():
