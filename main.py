62569af17152d9851d0776e300beff3fdc1d955d
Matthew Benedict <46784000+UInt2048@users.noreply.github.com>
2024-10-06 00:26:38 +0000

    Refactor stuff

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ app = Flask(__name__)
 
 def get_data():
      return [{"name": "Student"},
-     {"title": "Python hello", "days": random.randint(0, 7), "limit": 7, "type": "physical"},
-     {"title": "Mental health goal", "days": random.randint(0, 7), "limit": 7, "type": "mental"}]
+     {"goalID": 10, "title": "Python hello", "days": random.randint(0, 7), "limit": 7, "type": "physical"},
+     {"goalID": 20, "title": "Mental health goal", "days": random.randint(0, 7), "limit": 7, "type": "mental"}]
 
 @app.route("/add", methods=['GET', 'POST'])
 def add_data():
@@ -14,12 +14,14 @@ def add_data():
     name = request.args['name']
     goal_type = request.args['type']
     days = []
+    notifs = int(request.args['notifs']) != 0
+    weeks = request.args['weeks']
     for i in range(0, 7):
-        days.append(request.args["d%d" % i] == 0)
-    return add_data_internal(name, goal_type, days)
+        days.append(int(request.args["d%d" % i]) != 0)
+    return add_data_internal(name, goal_type, days, notifs, weeks)
 
-def add_data_internal(name: str, goal_type: str, days: list[bool]):
-    print("We're adding {} {} {}".format(name, goal_type, days))
+def add_data_internal(name: str, goal_type: str, days: list[bool], notifs: bool, weeks: int):
+    print("We're adding {}, {}, {}, {}, {}".format(name, goal_type, days, notifs, weeks))
     return main()
 
 @app.route("/update", methods=['GET', 'POST'])
@@ -27,8 +29,8 @@ def update_data():
     """Do something to update data"""
     return update_data_internal(int(request.args['goal']))
 
-def update_data_internal(goal: int):
-    print("We're updating goal number %d" % goal)
+def update_data_internal(goal_id: int):
+    print("We're updating goal number %d" % goal_id)
     return main()
 
 @app.route("/")
@@ -36,6 +38,9 @@ def main():
      """Main page of the app"""
      # Front-end conversion for display
      our_data = get_data()
+     profile_data = our_data[0]
+     incomplete_goals = []
+     complete_goals = []
      for index in range(1, len(our_data)):
          our_type = our_data[index].pop("type", None)
          match our_type:
@@ -43,5 +48,9 @@ def main():
                  our_data[index]["color"] = "#cc0035"
              case "mental":
                  our_data[index]["color"] = "#354ca1"
-     return render_template("index.html", data = our_data)
+         if (our_data[index]["days"] == our_data[index]["limit"]):
+             complete_goals.append(our_data[index])
+         else:
+             incomplete_goals.append(our_data[index])
+     return render_template("index.html", profile_data = profile_data, incomplete_goals = incomplete_goals, complete_goals = complete_goals)
 
